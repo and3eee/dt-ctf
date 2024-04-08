6ae18ad9fc34e9aaf98883c1f1e00f746701d454
Ryan <ryan.schueck@dynatrace.com>
2024-04-08 11:35:05 -0400

    updated counter to strato UI

--- components/counter.tsx
+++ components/counter.tsx
@@ -1,13 +1,14 @@
 "use client";
 
+import { Button } from "@dynatrace/strato-components-preview";
 import { useState } from "react";
-import { Button } from "@nextui-org/button";
+
 
 export const Counter = () => {
 	const [count, setCount] = useState(0);
 
 	return (
-		<Button radius="full" onPress={() => setCount(count + 1)}>
+		<Button  onClick={() => setCount(count + 1)}>
 			Count is {count}
 		</Button>
 	);
