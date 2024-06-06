c6afd434e175bfa554d05cc65eecd443a9486b90
Andreas Eickhoff <andreas.eickhoff@dynatrace.com>
2024-06-06 13:45:02 -0400

    Fixed auth issue

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,9 +1,15 @@
+"use server"
 import AuthCheck from "@/components/Auth/AuthCheck";
 import { auth } from "../api/auth/[...nextauth]/route";
 import UserModal from "@/components/User/UserModal";
 import { Stack } from "@mantine/core";
 import { prisma } from "@/lib/prisma";
 import UserInfo from "@/components/User/UserInfo";
+
+
+export const dynamic = "force-dynamic";
+export const revalidate = 30;
+
 
 export default async function AccountPage() {
   const session = await auth();
