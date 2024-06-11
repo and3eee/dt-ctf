252dc22cf3d05808b43d0c050404b3b185d05c99
Andreas Eickhoff <andreas.eickhoff@dynatrace.com>
2024-06-11 16:21:46 -0400

    Added members to leaderboard

--- components/Event/EventLeaderBoard.tsx
+++ components/Event/EventLeaderBoard.tsx
@@ -2,7 +2,7 @@
 
 import { EventProps, EventRiddleProps, TeamProps } from "@/types";
 
-import { Event, TeamEntry } from "@prisma/client";
+import { Event, TeamEntry, User } from "@prisma/client";
 import router from "next/router";
 import React from "react";
 import AuthCheck from "../Auth/AuthCheck";
@@ -10,7 +10,7 @@ import { registerMember } from "../Team/TeamControl";
 import TeamModal from "../Team/TeamModal";
 import { useAsyncList } from "@react-stately/data";
 import { GetTeams, GetTeamsRaw } from "./EventControl";
-import { Chip, Table } from "@mantine/core";
+import { Avatar, Chip, Table, Tooltip } from "@mantine/core";
 import { TableHeader, TableBody } from "react-stately";
 
 export default function EventLeaderBoard(props: {
@@ -22,10 +22,30 @@ export default function EventLeaderBoard(props: {
     (teamA: TeamProps, teamB: TeamProps) =>
       teamB.userEntries?.length! - teamA.userEntries?.length!
   );
+
+
+
+  const teamMembers = (team: TeamProps) => {
+    return (
+      <Tooltip.Group openDelay={300} closeDelay={100}>
+        <Avatar.Group>
+          {team.members?.map((user: User) => (
+            <Tooltip key={user.name} label={user.name!} withArrow>
+              <Avatar src={user.image}>
+                {user.name!.split(" ").map((word: string) => word.charAt(0))}
+              </Avatar>
+            </Tooltip>
+          ))}
+        </Avatar.Group>{" "}
+      </Tooltip.Group>
+    );
+  };
+
   const rows = teams.map((team: TeamProps) => (
     <Table.Tr key={team.name}>
       <Table.Td>{teams.indexOf(team) + 1}</Table.Td>
       <Table.Td>{team.name}</Table.Td>
+      <Table.Td>{teamMembers(team)}</Table.Td>
       <Table.Td>
         {team.userEntries!.length} / {props.riddles.length}
       </Table.Td>
@@ -38,6 +58,7 @@ export default function EventLeaderBoard(props: {
         <Table.Tr>
           <Table.Th>Position</Table.Th>
           <Table.Th>Team Name</Table.Th>
+          <Table.Th>Members</Table.Th>
           <Table.Th>Riddles Completed</Table.Th>
         </Table.Tr>
       </Table.Thead>
